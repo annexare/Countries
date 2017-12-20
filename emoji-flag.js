484e86243d4c38df716c5786ff32cc4e71906d91
Dmytro <z@annexare.com>
2017-12-20 22:54:13 +0200

    Minor Emoji flag code update

--- emoji-flag.js
+++ emoji-flag.js
@@ -1,17 +1,16 @@
+const { ucs2 } = require('punycode');
 const
   // "Regional Indicator Symbol Letter A" - "Latin Capital Letter A"
   UNICODE_BASE = 127462 - 'A'.charCodeAt(0),
   // Country code should contain exactly 2 uppercase characters from A..Z
-  COUNTRY_CODE_REGEX = /^[A-Z]{2}$/,
-
-  punycode = require('punycode');
+  COUNTRY_CODE_REGEX = /^[A-Z]{2}$/;
 
 const getEmojiFlag = (countryCode) => {
   if (!COUNTRY_CODE_REGEX.test(countryCode)) {
     return '';
   }
 
-  return punycode.ucs2.encode(
+  return ucs2.encode(
     countryCode
       .split('')
       .map(letter => UNICODE_BASE + letter.charCodeAt(0))
@@ -19,7 +18,7 @@ const getEmojiFlag = (countryCode) => {
 };
 
 const getUnicode = (emoji) => {
-  return punycode.ucs2.decode(emoji)
+  return ucs2.decode(emoji)
     .map(code => 'U+' + Number(code).toString(16).toUpperCase())
     .join(' ');
 };
