e56f38cfac5a18b15262dd7114477e4ef2483301
Moritz Friedrich <m@9dev.de>
2021-01-06 18:38:26 +0100

    Proper PHP support (#74)
    * Create index.php
    
    * Added autoloader entry
    
    * Removed Emoji functions due to PHP complexity issues
    
    * Updated code style to match library
    
    * Added PHPUnit
    
    * Added basic test implementation

---
+++ dist/index.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Annexare\Countries;
+
+use JsonException;
+
+use function file_get_contents;
+use function json_decode;
+
+use const JSON_THROW_ON_ERROR;
+
+/**
+ * Loads a JSON file relative to the current directory.
+ *
+ * @param string $path
+ *
+ * @return array
+ * @throws JsonException
+ * @internal
+ */
+function load(string $path): array
+{
+  static $cache = [];
+
+  if (isset($cache[$path])) {
+    return $cache[$path];
+  }
+
+  return $cache[$path] = json_decode(
+    file_get_contents(__DIR__ . '/' . $path),
+    true,
+    512,
+    JSON_THROW_ON_ERROR
+  );
+}
+
+/**
+ * Continents, key-value object (key is alpha-2 code).
+ *
+ * @return array
+ * @throws JsonException
+ */
+function continents(): array
+{
+  return load('continents.min.json');
+}
+
+/**
+ * Countries, key-value object (key is alpha-2 code, uppercase).
+ *
+ * @return array
+ * @throws JsonException
+ */
+function countries(): array
+{
+  return load('countries.min.json');
+}
+
+/**
+ * Languages in use only, key-value object (key is alpha-2 code).
+ *
+ * @return array
+ * @throws JsonException
+ */
+function languages(): array
+{
+  return load('languages.min.json');
+}
+
+/**
+ * Languages, key-value object (key is alpha-2 code).
+ * A complete list including not used by Countries list.
+ *
+ * @return array
+ * @throws JsonException
+ */
+function languagesAll(): array
+{
+  return load('languages.all.min.json');
+}
