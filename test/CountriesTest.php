1b85751441c9bc7774195f185b8fe34ffb122bcd
Dmytro <1391015+dmythro@users.noreply.github.com>
2021-01-08 18:56:14 +0200

    Proper PHP support + CI (#77)
    * Proper PHP support (#74)
    
    * Create index.php
    
    * Added autoloader entry
    
    * Removed Emoji functions due to PHP complexity issues
    
    * Updated code style to match library
    
    * Added PHPUnit
    
    * Added basic test implementation
    
    * ci: check php-actions/phpunit@v2
    
    * ci: remove nodejs 10
    
    * ci: PHP config updates
    
    * ci: nodejs 10 lives till about October '21 :)
    
    * v2.6.0
    
    Co-authored-by: Moritz Friedrich <m@9dev.de>

---
+++ test/CountriesTest.php
@@ -0,0 +1,76 @@
+<?php
+
+use PHPUnit\Framework\ExpectationFailedException;
+use PHPUnit\Framework\TestCase;
+
+use function Annexare\Countries\continents;
+use function Annexare\Countries\countries;
+use function Annexare\Countries\languages;
+use function Annexare\Countries\languagesAll;
+
+class CountriesTest extends TestCase
+{
+  /**
+   * @throws JsonException
+   * @throws ExpectationFailedException
+   * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
+   */
+  public function testCountries(): void
+  {
+    $countries = countries();
+
+    self::assertIsArray($countries);
+    self::assertJsonStringEqualsJsonFile(
+      __DIR__ . '/../dist/countries.min.json',
+      json_encode($countries, JSON_THROW_ON_ERROR)
+    );
+  }
+
+  /**
+   * @throws JsonException
+   * @throws ExpectationFailedException
+   * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
+   */
+  public function testContinents(): void
+  {
+    $continents = continents();
+
+    self::assertIsArray($continents);
+    self::assertJsonStringEqualsJsonFile(
+      __DIR__ . '/../dist/continents.min.json',
+      json_encode($continents, JSON_THROW_ON_ERROR)
+    );
+  }
+
+  /**
+   * @throws JsonException
+   * @throws ExpectationFailedException
+   * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
+   */
+  public function testLanguages(): void
+  {
+    $languages = languages();
+
+    self::assertIsArray($languages);
+    self::assertJsonStringEqualsJsonFile(
+      __DIR__ . '/../dist/languages.min.json',
+      json_encode($languages, JSON_THROW_ON_ERROR)
+    );
+  }
+
+  /**
+   * @throws JsonException
+   * @throws ExpectationFailedException
+   * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
+   */
+  public function testLanguagesAll(): void
+  {
+    $languagesAll = languagesAll();
+
+    self::assertIsArray($languagesAll);
+    self::assertJsonStringEqualsJsonFile(
+      __DIR__ . '/../dist/languages.all.min.json',
+      json_encode($languagesAll, JSON_THROW_ON_ERROR)
+    );
+  }
+}
