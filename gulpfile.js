6e6ff91616193efef9bad44301f9ec646eb902eb
Dmytro <d@klmn.me>
2021-03-10 20:38:53 +0200

    feat: remove migrated code

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,14 @@
 'use strict'
 
-// const path = require('path')
-
 const CONTINENTS = 'continents',
   COUNTRIES = 'countries',
   LANGUAGES = 'languages',
-  COMMA = '","',
   LF = '\n',
-  QUOTE = '"',
-  ALL = 'all',
   DATA = './data/',
   DATA_FILE = 'data',
   DIST = './dist/',
-  DO_CSV = 'csv',
-  DO_COPY = 'copy',
   DO_DATA = 'data',
-  DO_D_TS = 'd-ts',
   DO_EMOJI = 'emoji',
-  DO_MIN = 'min',
-  // DO_MIN_ES5 = 'es5.min',
   DO_MINIMAL = 'minimal',
   DO_SQL = 'sql',
   JSON_EXT = 'json',
@@ -29,119 +19,11 @@ const CONTINENTS = 'continents',
   countries = require(DATA + COUNTRIES + '.json'),
   languages = require(DATA + LANGUAGES + '.json'),
   DEFAULT_TASKS = [
-    DO_COPY,
-    DO_CSV,
     DO_DATA,
     DO_EMOJI,
     DO_MINIMAL,
-    DO_MIN,
     DO_SQL,
-    // DO_MIN_ES5,
-    DO_D_TS,
   ]
-
-const languagesInUse = getLanguagesInUse()
-
-/*
-gulp.task('test', function () {
-  const unused = [];
-  Object.keys(languages).forEach(lang => {
-    if (!languagesInUse.hasOwnProperty(lang)) {
-      unused.push(lang);
-    }
-  });
-
-  console.log(`Unused languages: ${unused.length} / ${Object.keys(languages).length}`);
-  console.error(JSON.stringify(unused));
-  console.info(JSON.stringify(Object.keys(languagesInUse)));
-});
-*/
-
-exports[DO_COPY] = function copy(callback) {
-  fs.writeFileSync(
-    `${DIST}${CONTINENTS}.${JSON_EXT}`,
-    JSON.stringify(continents, false, JSON_TAB) + LF
-  )
-  fs.writeFileSync(
-    `${DIST}${COUNTRIES}.${JSON_EXT}`,
-    JSON.stringify(countries, false, JSON_TAB) + LF
-  )
-  fs.writeFileSync(
-    `${DIST}${LANGUAGES}.${JSON_EXT}`,
-    JSON.stringify(languagesInUse, false, JSON_TAB) + LF
-  )
-  fs.writeFileSync(
-    `${DIST}${LANGUAGES}.${ALL}.${JSON_EXT}`,
-    JSON.stringify(languages, false, JSON_TAB) + LF
-  )
-  callback && callback()
-}
-
-exports[DO_CSV] = function csv(callback) {
-  const countryList = Object.keys(countries)
-  const csvHeader =
-    QUOTE +
-    'Code' +
-    COMMA +
-    Object.keys(countries.UA)
-      .map((key) => titleCase(key))
-      .join(COMMA) +
-    QUOTE
-  const csvData =
-    csvHeader +
-    LF +
-    countryList
-      .map((code) => {
-        const country = Object.assign({}, countries[code])
-        country.continent = continents[country.continent]
-        country.languages = getStringFromArray(country.languages)
-
-        return (
-          QUOTE + code + COMMA + objectValues(getCountryDataOrdered(country)).join(COMMA) + QUOTE
-        )
-      })
-      .join(LF)
-
-  fs.writeFile(`${DIST}${COUNTRIES}.${DO_CSV}`, csvData + LF, callback)
-}
-
-exports[DO_D_TS] = function d_ts(callback) {
-  const { name, version } = require('./package.json')
-  const [maj, min] = version.split('.')
-  let tpl = fs.readFileSync('./index.tpl.d.ts', 'utf8')
-
-  let continentList = ''
-  Object.keys(continents).forEach((continent) => {
-    continentList += `  ${continent}: string\n`
-  })
-
-  let countryList = ''
-  Object.keys(countries).forEach((country) => {
-    countryList += `  ${country}: Country\n`
-  })
-
-  let languageList = ''
-  Object.keys(languagesInUse).forEach((lang) => {
-    languageList += `  ${lang}: Language\n`
-  })
-
-  let languageAllList = ''
-  Object.keys(languages).forEach((langa) => {
-    languageAllList += `  ${langa}: Language\n`
-  })
-
-  fs.writeFileSync(
-    `${DIST}index.d.ts`,
-    tpl
-      .replace('// name', name)
-      .replace('// version', `${maj}.${min}`)
-      .replace(/\s\s\/\/ continents\s+/, continentList)
-      .replace(/\s\s\/\/ countries\s+/, countryList)
-      .replace(/\s\s\/\/ languages\s+/, languageList)
-      .replace(/\s\s\/\/ languagesAll\s+/, languageAllList)
-  )
-  callback && callback()
-}
 
 exports[DO_DATA] = function data(callback) {
   const fullData = {
@@ -168,31 +50,6 @@ exports[DO_EMOJI] = function emoji(callback) {
   fs.writeFileSync(
     `${DIST}${COUNTRIES}.${DO_EMOJI}.${DO_MIN}.${JSON_EXT}`,
     JSON.stringify(countriesEmoji) + LF
-  )
-  callback && callback()
-}
-
-// exports[DO_MIN_ES5] = function min_es5(callback) {
-//   const rollup = require('rollup')
-//   const loadConfigFile = require('rollup/dist/loadConfigFile')
-
-//   let outputOptions
-
-//   loadConfigFile(path.resolve(__dirname, 'rollup.config.js'))
-//     .then(({ options: [inputOptions] }) => {
-//       outputOptions = inputOptions.output[0]
-//       return rollup.rollup(inputOptions).then((bundle) => bundle.write(outputOptions))
-//     })
-//     .finally(callback)
-// }
-
-exports[DO_MIN] = function min(callback) {
-  fs.writeFileSync(`${DIST}${COUNTRIES}.${DO_MIN}.${JSON_EXT}`, JSON.stringify(countries) + LF)
-  fs.writeFileSync(`${DIST}${CONTINENTS}.${DO_MIN}.${JSON_EXT}`, JSON.stringify(continents) + LF)
-  fs.writeFileSync(`${DIST}${LANGUAGES}.${DO_MIN}.${JSON_EXT}`, JSON.stringify(languagesInUse) + LF)
-  fs.writeFileSync(
-    `${DIST}${LANGUAGES}.${ALL}.${DO_MIN}.${JSON_EXT}`,
-    JSON.stringify(languages) + LF
   )
   callback && callback()
 }
@@ -410,45 +267,6 @@ function getStringFromArray(arr) {
   }
 
   return arr.join(',')
-}
-
-function getLanguagesInUse() {
-  const usedList = []
-  Object.keys(countries).forEach((code) => {
-    const country = countries[code]
-    if (country.languages && country.languages.length) {
-      country.languages.forEach((lang) => {
-        if (usedList.indexOf(lang) < 0) {
-          usedList.push(lang)
-        }
-      })
-    }
-  })
-
-  const languagesInUse = {}
-  usedList.sort().forEach((lang) => {
-    languagesInUse[lang] = Object.assign({}, languages[lang])
-  })
-
-  // const unused = [];
-  // Object.keys(languages).forEach(lang => {
-  //   if (used.indexOf(lang) < 0) {
-  //     unused.push(lang);
-  //   }
-  // });
-
-  return languagesInUse
-}
-
-function objectValues(item) {
-  return Object.keys(item).map((key) => item[key])
-}
-function titleCase(str) {
-  str = str.toLowerCase().split(' ')
-  for (let i = 0; i < str.length; i++) {
-    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1)
-  }
-  return str.join(' ')
 }
 
 function sqlHeader(table, fields) {
