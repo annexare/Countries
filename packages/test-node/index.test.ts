e80ecc9269435e710e8192ccabd04a2da52b7f45
Dmytro <d@klmn.me>
2023-05-06 17:45:40 +0300

    CJS, ESM, IIFE builds; turbo task order for CI

--- packages/test-node/index.test.ts
+++ packages/test-node/index.test.ts
@@ -1,7 +1,9 @@
 import fs from 'node:fs'
 
-import * as es5 from '../../dist/index.es5.min.js'
+// @ts-ignore
+import * as cjs from '../../dist/index.js'
 import { continents, countries, languages, getEmojiFlag } from 'src/index.ts'
+import '../../dist/index.iife.d.ts'
 
 const source = {
   continents,
@@ -14,16 +16,16 @@ const exportDataList: Partial<keyof typeof source>[] = ['continents', 'countries
 const exportFnList: Partial<keyof typeof source>[] = ['getEmojiFlag']
 
 test('has proper ES5 export', () => {
-  expect(typeof es5).toBe('object')
+  expect(typeof cjs).toBe('object')
 
   for (const prop of exportFnList) {
-    expect(es5).toHaveProperty(prop)
+    expect(cjs).toHaveProperty(prop)
   }
 
   for (const prop of exportDataList) {
-    expect(es5).toHaveProperty(prop)
+    expect(cjs).toHaveProperty(prop)
 
-    const es5Props = Object.keys(es5[prop])
+    const es5Props = Object.keys(cjs[prop])
     const dataProps = Object.keys(source[prop])
     expect(es5Props).toEqual(dataProps)
   }
@@ -31,7 +33,7 @@ test('has proper ES5 export', () => {
 
 test('loads ES5 <script> properly', () => {
   const scriptEl = document.createElement('script')
-  scriptEl.text = fs.readFileSync('../../dist/index.es5.min.js', { encoding: 'utf-8' })
+  scriptEl.text = fs.readFileSync('../../dist/index.iife.js', { encoding: 'utf-8' })
   document.body.appendChild(scriptEl)
 
   expect(window.Countries).toBeDefined()
