5ebe32adcb74a2c77b89b158dd0953961bc09e1a
Dmytro <d@klmn.me>
2024-03-04 17:45:45 +0200

    Case insensitive search in getCountryCode function #131

--- packages/test-node/getCountryCode.test.ts
+++ packages/test-node/getCountryCode.test.ts
@@ -5,5 +5,11 @@ import { getCountryCode } from 'src/getCountryCode.ts'
 
 test('getCountryCode()', () => {
   assert.equal(getCountryCode('Ukraine'), 'UA')
+  assert.equal(getCountryCode('uKraine'), 'UA')
   assert.equal(getCountryCode('Україна'), 'UA')
+  assert.equal(getCountryCode('уКраїна'), 'UA')
+
+  assert.equal(getCountryCode('Ukrain'), false)
+  assert.equal(getCountryCode('Ukraine1'), false)
+  assert.equal(getCountryCode('Unknown'), false)
 })
