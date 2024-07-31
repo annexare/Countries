522b2ebf8eca2e6c38b9746aa1135b544cad5160
Dmytro <d@klmn.me>
2024-07-31 13:49:26 +0300

    refactor: Improve getCountryCode function
    Refactor the getCountryCode function to handle special characters and symbols in country names. This ensures that the function can match country names accurately, even when they contain special characters or symbols. The function now escapes special RegExp characters and replaces hyphens with the appropriate escape sequence. Additionally, the function now trims leading and trailing spaces from the country name before matching it against the country data list.

--- packages/test-node/getCountryCode.test.ts
+++ packages/test-node/getCountryCode.test.ts
@@ -12,4 +12,15 @@ test('getCountryCode()', () => {
   assert.equal(getCountryCode('Ukrain'), false)
   assert.equal(getCountryCode('Ukraine1'), false)
   assert.equal(getCountryCode('Unknown'), false)
+
+  // Should not care about leading/trailing spaces
+  assert.equal(getCountryCode(' Ukraine '), 'UA')
+
+  // More unicode tests
+  assert.equal(getCountryCode('မြန်မာ'), 'MM')
+  assert.equal(getCountryCode('澳門'), 'MO')
+
+  // Special symbols
+  assert.equal(getCountryCode('Myanmar (Burma)'), 'MM')
+  assert.equal(getCountryCode('Cocos (Keeling) Islands'), 'CC')
 })
