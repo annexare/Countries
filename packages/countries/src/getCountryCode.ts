3c991cb6251852f03e5eb598341aa30c208449ea
Dmytro <d@klmn.me>
2024-07-31 13:49:26 +0300

    refactor: Improve getCountryCode function
    Refactor the getCountryCode function to handle special characters and symbols in country names. This ensures that the function can match country names accurately, even when they contain special characters or symbols. The function now escapes special RegExp characters and replaces hyphens with the appropriate escape sequence. Additionally, the function now trims leading and trailing spaces from the country name before matching it against the country data list.

--- packages/countries/src/getCountryCode.ts
+++ packages/countries/src/getCountryCode.ts
@@ -4,8 +4,14 @@ import { getCountryDataList } from './getCountryData.ts'
 const countryDataList = getCountryDataList()
 
 export const getCountryCode = (countryName: string): TCountryCode | false => {
+  // Escape special RegExp characters
+  const name = `${countryName}`
+    .trim()
+    .replace(/[|\\{}()[\]^$+*?.]/g, '\\$&')
+    .replace(/-/g, '\\x2d')
+
   // Match exact country name, but case insensitive
-  const nameRegex = new RegExp('^' + countryName + '$', 'i')
+  const nameRegex = new RegExp('^' + name + '$', 'i')
 
   return (
     countryDataList.find(({ name, native }) => nameRegex.test(name) || nameRegex.test(native))
